f8bbb00b57c927db8f55cdb380cc5ea9464ee1d3
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-06 12:07:12 +0900

    snippet serializer 구현 / django_extensions app 추가

--- drf_tutorial/snippets/models.py
+++ drf_tutorial/snippets/models.py
@@ -13,7 +13,8 @@ class Snippet(models.Model):
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='tango', max_length=100)
 
     class Meta:
         ordering = ('created',)
+
