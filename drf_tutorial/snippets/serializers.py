46e86664be7454e291356b21d319f0e68afbdd75
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-06 13:54:18 +0900

    serializer를 modelserializer로 구현

--- drf_tutorial/snippets/serializers.py
+++ drf_tutorial/snippets/serializers.py
@@ -3,33 +3,14 @@ from rest_framework import serializers
 from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=150)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='tango')
-
-    def create(self, validated_data):
-        """
-        Receives a dictionary validated_data that has all the fields of SnippetSerializer and creates the model object Snippet
-        :param validated_data:
-        :return:
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-
-        :param instance:
-        :param validated_data:
-        :return:
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = (
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+        )
