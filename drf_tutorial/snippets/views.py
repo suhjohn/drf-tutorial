dd6814989bb75cb89bff93fd9a8434dfec9ded49
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-06 14:01:59 +0900

    add snippets_list view / add url

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+
+def snippet_list(request):
+    """
+
+    :param request:
+    :return:
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
